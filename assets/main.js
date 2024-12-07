f219238d65a8e0429b16123ab06c6eb4a4d5c2ad
Alexander <maina206mwangi@gmail.com>
2024-12-07 21:58:51 +0300

    Finished version

--- assets/main.js
+++ assets/main.js
@@ -1,1 +1,52 @@
+/*=============== EMAIL JS ===============*/
 
+/*=============== SHOW SCROLL UP ===============*/
+const scrollUp = () => {
+  const scrollUp = document.getElementById("scroll-up");
+  this.scrollY >= 350
+    ? scrollUp.classList.add("show-scroll")
+    : scrollUp.classList.remove("show-scroll");
+};
+
+window.addEventListener("scroll", scrollUp);
+
+/*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+/* This part of our code helps us create a responsive style in the Nav when you scroll from section to section*/
+const sections = document.querySelectorAll("section[id]");
+
+const scrollActive = () => {
+  const scrollDown = window.scrollY;
+
+  sections.forEach((current) => {
+    const sectionHeight = current.offsetHeight;
+    const sectionTop = current.offsetTop - 58;
+    const sectionId = current.getAttribute("id");
+    const sectionClass = document.querySelector(
+      ".nav__list a[href*=" + sectionId + "]"
+    );
+
+    if (scrollDown > sectionTop && scrollDown <= sectionTop + sectionHeight) {
+      sectionClass.classList.add("active-link");
+    } else {
+      sectionClass.classList.remove("active-link");
+    }
+  });
+};
+window.addEventListener("scroll", scrollActive);
+
+/*=============== SCROLL REVEAL ANIMATION ===============*/
+const sr = ScrollReveal({
+  origin: "top",
+  distance: "60px",
+  duration: 2500,
+  delay: 400,
+  //   reset: true, //Animations repeat
+});
+
+sr.reveal(`.perfil, .contact__form`);
+sr.reveal(`.info`, { origin: "left", delay: 800 });
+sr.reveal(`.skills`, { origin: "left", delay: 1000 });
+sr.reveal(`.about`, { origin: "right", delay: 1200 });
+sr.reveal(`.projects__card, .services__card, .experience__card`, {
+  interval: 100,
+});
